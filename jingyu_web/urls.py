580a5e6a4c887a0771e38a5798de154ed2304414
jingyu@digitalocean <root@jingyuliu.com>
2014-06-20 16:54:02 -0400

    first commit of all working skeleton on deploy

--- jingyu_web/urls.py
+++ jingyu_web/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'jingyu_web.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^', include('hello.urls')),
+
+    # Static pages
+    url(r'^$', include('hello.urls')),
+    url(r'^about$', TemplateView.as_view(template_name='hello/about.html'), name='about'),
+    url(r'^work$', TemplateView.as_view(template_name='hello/work.html'), name='work'),
+    url(r'^blog$', TemplateView.as_view(template_name='hello/blog.html'), name='blog'),
+    url(r'^contact$', TemplateView.as_view(template_name='hello/contact.html'), name='contact'),
+    
+
+    # Admin site
     url(r'^admin/', include(admin.site.urls)),
 )
